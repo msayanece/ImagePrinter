702ed9aa578e36380700faf91ea687f2d00c6139
Admin <anshu.p@encoders.co.in>
2017-07-11 13:32:44 +0530

    update code for print from xml layout

--- app/src/main/java/com/example/admin/imageprinter/MainActivity.java
+++ app/src/main/java/com/example/admin/imageprinter/MainActivity.java
@@ -1,26 +1,61 @@
 package com.example.admin.imageprinter;
 
+import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.print.PrintHelper;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    View mView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get the layout of visiting card
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.identity_card, null);
+
+        //get the main view which will be printed
+        RelativeLayout view = (RelativeLayout) contentView.findViewById(R.id.relative);
+
+        //get all the dynamic views which will be changed programmatically
+        TextView tv = (TextView) view.findViewById(R.id.textView3);
+
+        //change name
+        tv.setText("Titir Mukherjee");
+        view.setDrawingCacheEnabled(true);
+        // this is the important code
+        // Without it the view will have a dimension of 0,0 and the bitmap will be null
+        //set view hight, width
+        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        //layout(left, top, size from left to right, size from top to bottom)
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+
+        //build the cache with autoscale enabled
+        view.buildDrawingCache(true);
+
+        //store the view for further use
+        mView = view;
     }
 
     private void doPhotoPrint() {
         PrintHelper photoPrinter = new PrintHelper(this);
         photoPrinter.setScaleMode(PrintHelper.SCALE_MODE_FIT);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
-                R.drawable.image);
+        //this is used for print drawable image
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+//                R.drawable.image);
+
+        // get the layout in a bitmap
+        Bitmap bitmap = mView.getDrawingCache();
         photoPrinter.printBitmap("image.png_test_print", bitmap, new PrintHelper.OnPrintFinishCallback() {
             @Override
             public void onFinish() {

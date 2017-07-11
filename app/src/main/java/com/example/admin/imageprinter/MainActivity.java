fc9c71ede9a27ed3904d283cad294cb226af3501
Admin <anshu.p@encoders.co.in>
2017-07-11 10:54:28 +0530

    init commit

---
+++ app/src/main/java/com/example/admin/imageprinter/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.admin.imageprinter;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v4.print.PrintHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void doPhotoPrint() {
+        PrintHelper photoPrinter = new PrintHelper(this);
+        photoPrinter.setScaleMode(PrintHelper.SCALE_MODE_FIT);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.image);
+        photoPrinter.printBitmap("image.png_test_print", bitmap, new PrintHelper.OnPrintFinishCallback() {
+            @Override
+            public void onFinish() {
+                Toast.makeText(MainActivity.this, "Printing successful!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        doPhotoPrint();
+    }
+}
